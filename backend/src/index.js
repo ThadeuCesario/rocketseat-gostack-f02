3e25220ff295bbda8da4d7df3543aeeefbd3afd7
Thadeu Cesário <thadeu.cesario@icloud.com>
2020-04-06 22:45:52 -0300

    Gostack: backend rota projects

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,20 @@ const express = require('express');
 const app = express();
 
 /**
+ * Temos abaixo o método GET, lembrando que com o Express podemos controlar rotas.
+ * Para isso vamos utilizar o método 'get' do express. No primeiro parâmetro
+ * temos a rota que estaremos trabalhando que no caso é a '/project'.
+ * O segundo parâmetro é uma função. Veja que estou utilizando arrow function.
+ * Essa função utiliza de dois parâmetros, sendo que o primeiro é a requisição
+ * e o segundo é a resposta.
+ * O return dessa função é sempre uma resposta. Temos várias formas de retornar
+ * uma resposta. Porém a mais comum no início é o send().
+ */
+app.get('/projects', (request, response) => {
+  return response.send('Hello World');
+});
+
+/**
  * Com o App.listen, a aplicação estará em uma porta na qual podemos
  * acessar pelo localhost. Neste caso estará disponível na porta 3333.
  */

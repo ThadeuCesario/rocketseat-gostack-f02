082c3deef71dacf491481d22c19622eb1cf173f7
Thadeu Cesário <thadeu.cesario@icloud.com>
2020-04-06 22:38:20 -0300

    Gostack: backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,3 +1,9 @@
 const express = require('express');
 
 const app = express();
+
+/**
+ * Com o App.listen, a aplicação estará em uma porta na qual podemos
+ * acessar pelo localhost. Neste caso estará disponível na porta 3333.
+ */
+app.listen(3333); 

98e9f247923f9f91a50df08e3ff49d6be6443b73
Thadeu Cesário <thadeu.cesario@icloud.com>
2020-04-07 23:00:59 -0300

    setting routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const {uuid} = require('uuidv4'); //Responsável por criar um id único universal.
 const app = express();
 
 /**
@@ -65,18 +65,17 @@ app.use(express.json());
   * Essas informações na hora de criar/deletar/alterar, pegaremos através do
   * request body. Essas informações chegam através de JSON.
   */
+
+const projects = [];
+
 app.get('/projects', (request, response) => {
   /**
    * Tudo o que está dentro do request.query estão sendo enviados.
    */
-  const {title, owner} = request.query;
-  console.log(title);
-  console.log(owner);
+  const {title} = request.query;
+  const results = title ? projects.filter(project => project.title.includes(title)) : projects;
 
-  return response.json([
-    'Projeto 1',
-    'Projeto 2'
-  ]);
+  return response.json(results);
 });
 
 app.post('/projects', (request, response) =>{
@@ -90,14 +89,12 @@ app.post('/projects', (request, response) =>{
    */
 
   const {title, owner} = request.body;
-  console.log(title);
-  console.log(owner);
+  // console.log(title);
+  // console.log(owner);
+  const project = {id: uuid(), title, owner} 
+  projects.push(project); 
 
-  return response.json([
-    'Projeto 1',
-    'Projeto 2',
-    'Projeto 3'
-  ]);
+  return response.json(project);
 });
 
 /**
@@ -117,13 +114,42 @@ app.put('/projects/:id', (request, response) => {
    */
 
   const {id} = request.params;
-  console.log(id);
+  const {title, owner} = request.body;
+  // console.log(id);
 
-  return response.json([
-    'Projeto 4',
-    'Projeto 2',
-    'Projeto 3'
-  ]);
+  /**
+   * Abaixo estamos utilizando uma função find para percorrer um array executando
+   * uma função.
+   * No caso, estamos comparando cada project.id, com nosso id desestruturado.
+   * se eles forem iguais retornará true.
+   * 
+   * Alteramos para projectIndex, pois com a posição do projeto ficará mais fácil
+   * para atualizar a informação.
+   */
+  const projectIndex = projects.findIndex(project => project.id === id);
+
+  if(projectIndex < 0){
+    //Se não encontrou o índice, retornará -1 (menor do que zero)
+    //Veja que estou alterando também o status do servidor.
+
+    return response.status(400).json({ error: 'Project not found.'});
+  }
+
+  /**
+   * Agora vamos atualizar os dados, criando um novo objeto 'project'
+   * e logo abaixo acessamos o array passando o projectIndex no indíce 
+   * e alterando pelo project novo.
+   */
+
+  const project = {
+    id,
+    title,
+    owner
+  };
+
+  projects[projectIndex] = project;
+
+  return response.json(project);
 });
 
 /**
@@ -131,10 +157,18 @@ app.put('/projects/:id', (request, response) => {
  * um 'id' como parâmetro.
  */
 app.delete('/projects/:id', (request, response) => {
-  return response.json([
-    'Projeto 2',
-    'Projeto 3'
-  ]);
+  const {id} = request.params;
+  const projectIndex = projects.findIndex(project => project.id === id);
+
+  if(projectIndex < 0){
+    //Se não encontrou o índice, retornará -1 (menor do que zero)
+    //Veja que estou alterando também o status do servidor.
+
+    return response.status(400).json({ error: 'Project not found.'});
+  }
+
+  projects.splice(projectIndex, 1);
+  return response.status(204).send();
 })
 
 /**

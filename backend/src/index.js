6b3c878c62be7078b91f87bbbb022ea924b831ba
Thadeu Cesário <thadeu.cesario@icloud.com>
2020-04-08 21:12:59 -0300

    using middlewares

--- backend/src/index.js
+++ backend/src/index.js
@@ -118,7 +118,7 @@ function validateProjectId(request, response, next){
 }
 
 app.use(logRequests);
-app.use('/projects/:id', validateProjectId);
+app.use('/projects/:id', validateProjectId); 
 
 app.get('/projects', (request, response) => {
   console.log('3');
